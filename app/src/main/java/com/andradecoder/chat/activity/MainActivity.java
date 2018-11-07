5d0a43d31e65ec16180d07ceb1f032e8650b31ba
Lucas Andrade <lap.andrade15@gmail.com>
2018-11-07 16:02:44 -0300

    chat com firebase funcionando

--- app/src/main/java/com/andradecoder/chat/activity/MainActivity.java
+++ app/src/main/java/com/andradecoder/chat/activity/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        
         Toolbar toolbar = findViewById(R.id.toolbarChat);
         setSupportActionBar(toolbar);
 
